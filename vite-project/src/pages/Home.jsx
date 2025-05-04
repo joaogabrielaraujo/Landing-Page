b150c2a9b1aede50096ef3e25a3680cea12e0c8d
João Gabriel Araújo Almeida <112009180+joaogabrielaraujo@users.noreply.github.com>
2025-05-04 09:32:47 -0300

    ✨feat: Start new React project

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+      <p className="text-lg text-gray-700">This is a simple home page using Tailwind CSS.</p>
+    </div>
+  );
+}
